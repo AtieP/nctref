63af8cb6bec2757081151a303756acfce00a59ed
A A <245600-midn@users.noreply.gitlab.com>
2020-09-10 23:58:41 +0300

    Can now switch between nasm and at&t syntax

--- src/cg/x86.c
+++ src/cg/x86.c
@@ -2,6 +2,8 @@
 
 #include<stdlib.h>
 
+#define SYNTAX_GAS
+
 static uint8_t register_size(int i) {
 	return 1 << (i / 5);
 }
@@ -33,6 +35,16 @@ static RegisterAllocator *create_ralloc(X86 *X) {
 
 static char *yasm_typename(int bytes) {
 	switch(bytes) {
+#ifdef SYNTAX_GAS
+	case 1:
+		return "byte";
+	case 2:
+		return "word";
+	case 4:
+		return "long";
+	case 8:
+		return "quad";
+#else
 	case 1:
 		return "db";
 	case 2:
@@ -41,10 +53,51 @@ static char *yasm_typename(int bytes) {
 		return "dd";
 	case 8:
 		return "dq";
+#endif
 	}
 
 	abort();
 	return NULL;
+}
+
+static void gmovi(X86 *X, const char *dst, size_t val) {
+#ifdef SYNTAX_GAS
+	X->text = sdscatfmt(X->text, "mov $%i, %%%s\n", val, dst);
+#else
+	X->text = sdscatfmt(X->text, "mov $%i, %%%s\n", val, dst);
+#endif
+}
+
+static void gmovr(X86 *X, int zx, const char *dst, const char *src) {
+#ifdef SYNTAX_GAS
+	X->text = sdscatfmt(X->text, "mov%s %%%s, %%%s\n", zx ? "zx" : "", src, dst);
+#else
+	X->text = sdscatfmt(X->text, "mov%s %s, %s\n", zx ? "zx" : "", dst, src);
+#endif
+}
+
+static void gaddi(X86 *X, const char *dst, size_t val) {
+#ifdef SYNTAX_GAS
+	X->text = sdscatfmt(X->text, "add $%i, %%%s\n", val, dst);
+#else
+	X->text = sdscatfmt(X->text, "add %s, %i\n", dst, val);
+#endif
+}
+
+static void gaddr(X86 *X, const char *dst, const char *src) {
+#ifdef SYNTAX_GAS
+	X->text = sdscatfmt(X->text, "add %%%s, %%%s\n", src, dst);
+#else
+	X->text = sdscatfmt(X->text, "add %s, %s\n", dst, src);
+#endif
+}
+
+static void gtest1(X86 *X, const char *reg) {
+#ifdef SYNTAX_GAS
+	X->text = sdscatfmt(X->text, "test %%%s, %%%s\n", reg, reg);
+#else
+	X->text = sdscatfmt(X->text, "test %s, %s\n", reg, reg);
+#endif
 }
 
 void x86_new(X86 *X) {
@@ -64,7 +117,7 @@ int x86_visit_expression(X86 *X, AST *ast, size_t coerceToSize) {
 	if(ast->nodeKind == AST_EXPRESSION_PRIMITIVE) {
 		int dst = ralloc_alloc(X->rallocator, coerceToSize);
 
-		X->text = sdscatfmt(X->text, "mov %s, %i\n", X->rallocator->registers[dst].name, ast->expressionPrimitive.numerator / ast->expressionPrimitive.denominator);
+		gmovi(X, X->rallocator->registers[dst].name, ast->expressionPrimitive.numerator / ast->expressionPrimitive.denominator);
 
 		return dst;
 	} else if(ast->nodeKind == AST_EXPRESSION_VAR) {
@@ -74,7 +127,7 @@ int x86_visit_expression(X86 *X, AST *ast, size_t coerceToSize) {
 		// fixes "mov al, bx"
 		if(register_size(src) > register_size(dst)) src = cast_register(src, register_size(dst));
 		
-		X->text = sdscatfmt(X->text, "%s %s, %s\n", register_size(dst) > register_size(src) ? "movzx" : "mov", X->rallocator->registers[dst].name, X->rallocator->registers[src].name);
+		gmovr(X, register_size(dst) > register_size(src), X->rallocator->registers[dst].name, X->rallocator->registers[src].name);
 
 		return dst;
 	} else if(ast->nodeKind == AST_EXPRESSION_BINARY_OP) {
@@ -83,11 +136,11 @@ int x86_visit_expression(X86 *X, AST *ast, size_t coerceToSize) {
 		for(size_t i = 1; i < ast->expressionBinaryOp.amountOfOperands; i++) {
 			AST *operand = ast->expressionBinaryOp.operands[i];
 			if(operand->nodeKind == AST_EXPRESSION_PRIMITIVE) {
-				X->text = sdscatfmt(X->text, "add %s, %i\n", X->rallocator->registers[dst].name, operand->expressionPrimitive.numerator / operand->expressionPrimitive.denominator);
+				gaddi(X, X->rallocator->registers[dst].name, operand->expressionPrimitive.numerator / operand->expressionPrimitive.denominator);
 			} else if(operand->nodeKind == AST_EXPRESSION_VAR) {
 				int addend = ((X86VarEntryInfo*) operand->expressionVar.thing->userdata)->id;
 
-				X->text = sdscatfmt(X->text, "add %s, %s\n", X->rallocator->registers[dst].name, X->rallocator->registers[addend].name);
+				gaddr(X, X->rallocator->registers[dst].name, X->rallocator->registers[cast_register(addend, register_size(dst))].name);
 			}
 		}
 		
@@ -108,16 +161,20 @@ AST *x86_visit_statement(X86 *X, AST *ast) {
 			}
 			
 			if(!ent->data.symbol.isLocal) {
-				X->text = sdscatfmt(X->text, "global %s\n", ent->name);
+				X->text = sdscatfmt(X->text, ".global %s\n", ent->name);
 			}
 			
 			size_t typeSize = type_size(ent->type);
 			
 			if(ast->statementDecl.expression) {
 				AST *expr = ast->statementDecl.expression;
-				X->text = sdscatfmt(X->text, "%s: %s %i\n", ent->data.symbol.linkName, yasm_typename(typeSize), expr->expressionPrimitive.numerator / expr->expressionPrimitive.denominator);
+				X->text = sdscatfmt(X->text, "%s: .%s %i\n", ent->data.symbol.linkName, yasm_typename(typeSize), expr->expressionPrimitive.numerator / expr->expressionPrimitive.denominator);
 			} else {
+#ifdef SYNTAX_GAS
+				X->text = sdscatfmt(X->text, "%s: .skip %i\n", ent->data.symbol.linkName, typeSize);
+#else
 				X->text = sdscatfmt(X->text, "%s: resb %i\n", ent->data.symbol.linkName, typeSize);
+#endif
 			}
 		} else if(ent->kind == VARTABLEENTRY_VAR) {
 			X86VarEntryInfo *info = ent->userdata = malloc(sizeof(*info));
@@ -128,8 +185,16 @@ AST *x86_visit_statement(X86 *X, AST *ast) {
 		switch(ast->statementIf.expression->expression.constantType) {
 		case EXPRESSION_NOT_CONSTANT: {
 			const char *reg = X->rallocator->registers[x86_visit_expression(X, ast->statementIf.expression, 4)].name;
-			X->text = sdscatfmt(X->text, "test %s, %s\njz .L%i\n", reg, reg, X->lidx);
+
+			gtest1(X, reg);
+#ifdef SYNTAX_GAS
+			X->text = sdscatfmt(X->text, "jz $.L%i\n", X->lidx);
+#else
+			X->text = sdscatfmt(X->text, "jz .L%i\n", X->lidx);
+#endif
+
 			x86_visit_chunk(X, ast->statementIf.then);
+
 			X->text = sdscatfmt(X->text, ".L%i:", X->lidx++);
 			break;
 		}
@@ -156,5 +221,9 @@ void x86_visit_chunk(X86 *X, ASTChunk *chunk) {
 }
 
 void x86_finish(X86 *X) {
+#ifdef SYNTAX_GAS
+	fprintf(stdout, ".section .text\n%.*s", sdslen(X->text), X->text);
+#else
 	fprintf(stdout, "section .text\n%.*s", sdslen(X->text), X->text);
+#endif
 }
